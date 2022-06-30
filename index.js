d7215c2d459f89c2114bfc19dd4a965b9f91e923
krishna <bhattakrishna111@gmail.com>
2022-06-30 21:31:02 +0545

    Final project

---
+++ index.js
@@ -0,0 +1,101 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280/";
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+getMovies(APIURL);
+
+async function getMovies(url){
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+    
+
+    
+}
+
+function showMovies(movies){
+    main.innerHTML ="";
+    movies.forEach(movie =>{
+        const {poster_path, title, vote_average,overview} = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML =`
+        
+            <img src="${IMGPATH + poster_path}";
+            alt="${title}"/>
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+            <h4>Overview:</h4>
+                ${overview}
+            </div>
+
+       
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+
+function getClassByRate(vote){
+    if(vote>=8){
+        return 'green';
+
+    }else if(vote>=5){
+        return 'orange'
+    }else{
+        return 'red';
+    }
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value="";
+    }
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
